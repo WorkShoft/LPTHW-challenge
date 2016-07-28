a79b6f9d2d2f293148b2a2d1314c86ab8a0aad8b
Mikel Losada <mikel.losada@gmail.com>
2016-07-28 11:07:46 +0200

    Yet another round of scripts.

---
+++ ex22.py
@@ -0,0 +1,81 @@
+### FUNCTIONS ###
+'''
+    print
+    len
+    
+    raw_input()
+    open()
+    read()
+    truncate()
+    write()
+    close()
+    seek()
+    readline()
+    
+    add()
+    subtract()
+    multiply()
+    divide()
+    
+    (*args)
+   
+'''   
+
+### MODULES (LIBRARIES) ###
+'''
+
+sys/argv
+os.path/exists
+
+'''
+
+### SYMBOLS ###
+'''
+    + plus
+    - minus
+    / slash
+    * asterisk
+    % percent
+    < less-than
+    > greater-than
+    <= less-than-equal
+    >= greater-than-equal
+    
+    
+Escape 	    What it does.
+\\ 	        Backslash (\)
+\' 	        Single-quote (')
+\" 	        Double-quote (")
+\a 	        ASCII bell (BEL)
+\b 	        ASCII backspace (BS)
+\f 	        ASCII formfeed (FF)
+\n 	        ASCII linefeed (LF)
+\N{name} 	Character named name in the Unicode database (Unicode only)
+\r 	        Carriage Return (CR)
+\t 	        Horizontal Tab (TAB)
+\uxxxx 	    Character with 16-bit hex value xxxx (Unicode only)
+\Uxxxxxxxx 	Character with 32-bit hex value xxxxxxxx (Unicode only)
+\v 	        ASCII vertical tab (VT)
+\ooo 	    Character with octal value ooo
+\xhh 	    Character with hex value hh
+
+'''
+
+### WARNINGS AND TRICKS ###
+'''
+Remember to put # -*- coding: utf-8 -*- at the top if you use non-ASCII characters   and get an encoding error.
+
+Add strings with + 
+
+Use the %r for debugging, since it displays the "raw" data of the variable, but the others are used for displaying to users.
+
+try x = int(raw_input()) which gets the number as a string from raw_input() then converts it to an integer using int().
+
+ 
+command line arguments come in as strings, even if you typed numbers on the command line. Use int() to convert them just like with int(raw_input()).
+
+cat is an old command that "con*cat*enates" files together, but mostly it's just an easy way to print a file to the screen. Type man cat to read about it.
+
+'''
+
+
